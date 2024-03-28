f73d965b8c0c72796321b5f9a603fa5980540f55
Морозов Александр <@step.kz>
2024-03-28 19:40:34 +0500

    lesson_44

--- testapp/models.py
+++ testapp/models.py
@@ -6,8 +6,24 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.postgres.fields import DateTimeRangeField, ArrayField, HStoreField, CICharField  # , JSONField
 from django.contrib.postgres.indexes import GistIndex
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.db.models import JSONField
+
+
+class Profile(models.Model):
+    phone = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+# class AdvUser(AbstractUser):
+#     phone = models.CharField(max_length=20)
+
+
+# class AdvUser(User):
+#     phone = models.CharField(max_length=20)
+#
+#     class Meta:
+#         proxy = True
 
 
 class AdvUser(models.Model):
@@ -75,48 +91,48 @@ class PrivateMessage(Message):
 #     class Meta(Message.Meta):
 #         pass
 
-class PGSRoomReserving(models.Model):
-    name = models.CharField(max_length=20, verbose_name='Помещение')
-    reserving = DateTimeRangeField(verbose_name='Время резервирования')
-    cancelled = models.BooleanField(default=False, verbose_name='Отменить резервирование')
-
-    class Meta:
-        indexes = [
-            GistIndex(fields=['reserving'],
-                      name='i_pgsrr_reserving',
-                      opclasses=('range_ops',),
-                      fillfactor=50)
-        ]
-
-
-class PGSRubric(models.Model):
-    name = models.CharField(max_length=20, verbose_name='Имя')
-    description = models.TextField(verbose_name='Описание')
-    tags = ArrayField(base_field=models.CharField(max_length=20), verbose_name='Теги')
-
-    class Meta:
-        indexes = [
-            models.Index(fields=('name', 'description'),
-                         name='i_pgsrubric_name_description',
-                         opclasses=('varchar_pattern_ops', 'bpchar_pattern_ops'))
-        ]
-
-
-class PGSProject(models.Model):
-    name = models.CharField(max_length=40, verbose_name='Название')
-    platforms = ArrayField(base_field=ArrayField(
-        base_field=models.CharField(max_length=20)),
-        verbose_name='Используемые платформы')
-
-
-class PGSProject2(models.Model):
-    name = models.CharField(max_length=40, verbose_name='Название')
-    platforms = HStoreField(verbose_name='Используемые платформы')
-
-
-class PGSProject3(models.Model):
-    name = CICharField(max_length=40, verbose_name='Название')
-    data = JSONField()
+# class PGSRoomReserving(models.Model):
+#     name = models.CharField(max_length=20, verbose_name='Помещение')
+#     reserving = DateTimeRangeField(verbose_name='Время резервирования')
+#     cancelled = models.BooleanField(default=False, verbose_name='Отменить резервирование')
+#
+#     class Meta:
+#         indexes = [
+#             GistIndex(fields=['reserving'],
+#                       name='i_pgsrr_reserving',
+#                       opclasses=('range_ops',),
+#                       fillfactor=50)
+#         ]
+#
+#
+# class PGSRubric(models.Model):
+#     name = models.CharField(max_length=20, verbose_name='Имя')
+#     description = models.TextField(verbose_name='Описание')
+#     tags = ArrayField(base_field=models.CharField(max_length=20), verbose_name='Теги')
+#
+#     class Meta:
+#         indexes = [
+#             models.Index(fields=('name', 'description'),
+#                          name='i_pgsrubric_name_description',
+#                          opclasses=('varchar_pattern_ops', 'bpchar_pattern_ops'))
+#         ]
+#
+#
+# class PGSProject(models.Model):
+#     name = models.CharField(max_length=40, verbose_name='Название')
+#     platforms = ArrayField(base_field=ArrayField(
+#         base_field=models.CharField(max_length=20)),
+#         verbose_name='Используемые платформы')
+#
+#
+# class PGSProject2(models.Model):
+#     name = models.CharField(max_length=40, verbose_name='Название')
+#     platforms = HStoreField(verbose_name='Используемые платформы')
+#
+#
+# class PGSProject3(models.Model):
+#     name = CICharField(max_length=40, verbose_name='Название')
+#     data = JSONField()
 
 
 def get_timestamp_path(instance, filename):
